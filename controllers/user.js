930003b94008fabc3aab5ec3f384b94bbebe1b4e
Soham Kudalkar <sohamskudalkar02@gmail.com>
2023-05-15 19:17:16 +0530

    Added basic login capability

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,28 @@
 const { userLogin } = require("../models/userlogin");
 
 async function handleRegister(req, res) {
-  const reg = await userLogin.create({id: req.body.email, pword: req.body.password})
-  res.send(JSON.stringify({ status: "sucess", code: 01 }));
+  try {
+    const reg = await userLogin.create({
+      id: req.body.email,
+      pword: req.body.password,
+    });
+    res.send(JSON.stringify({ status: "sucess", code: 01 }));
+  } catch (error) {
+    if (error.name === "SequelizeUniqueConstraintError") {
+      res.send(JSON.stringify({ status: "failure", code: 03 }));
+    }
+  }
 }
 
-async function handleLogin() {}
+async function handleLogin(req, res) {
+  const resp = await userLogin.findOne({
+    where: { id: req.body.email, pword: req.body.password },
+  });
+
+  if (resp) {
+    res.send(JSON.stringify({ status: "login Successfull", code: 01 }));
+  } else res.send(JSON.stringify({ status: "login failed", code: 02 }));
+}
 
 module.exports = {
   handleRegister,
--- static/js/auth.js
+++ static/js/auth.js
@@ -6,8 +6,28 @@ document
       email: document.querySelector("[data-work = username]").value,
       password: document.querySelector("[data-work = password]").value,
     });
-
     const resp = await fetch("/v1/register", {
+      method: "POST",
+      body: body,
+      headers: {
+        "content-type": "application/json; charset=UTF-8",
+      },
+    });
+    let js = await resp.json();
+    if (js.code == 02) console.error(js.status);
+    else if (js.code == 03) console.error("User Already Exists");
+    else console.log(js.status);
+  });
+
+document
+  .querySelector("[data-work = login]")
+  .addEventListener("click", async (e) => {
+    e.preventDefault();
+    let body = JSON.stringify({
+      email: document.querySelector("[data-work = username]").value,
+      password: document.querySelector("[data-work = password]").value,
+    });
+    const resp = await fetch("/v1/login", {
       method: "POST",
       body: body,
       headers: {
@@ -18,8 +38,3 @@ document
     if (js.code == 02) console.error(js.status);
     else console.log(js.status);
   });
-
-document.querySelector("[data-work = login]").addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log("test2");
-});
